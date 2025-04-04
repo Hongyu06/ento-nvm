24dbb03933bb9898b2dba05e47c1f02445efde1d
Hongyu06 <hola581303@gmail.com>
2025-04-04 10:27:00 +0200

    editado+-

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          No<code>sortira la</code> vandal chonovoid.
         </p>
         <a
           className="App-link"
