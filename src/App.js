053fa4b6c59ab0064bf22a8d63dc20ce1b8f7f97
Hongyu06 <hola581303@gmail.com>
2025-04-04 10:30:14 +0200

    canvio link

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          No<code>sortira la</code> vandal chonovoid.
+          No et <code>sortira la</code> vandal chonovoid.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://genshin.hoyoverse.com/es/home"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cosa God
         </a>
       </header>
     </div>
